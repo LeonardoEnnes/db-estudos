aae02fadfd6000348c8c01e8fef262325344f456
Leonardo Ennes <leonardoennes7@gmail.com>
2026-05-29 10:59:30 -0300

    feat: adiciona novos campos e relacionamentos na entidade Address e Person

--- Spring/person-api/src/main/java/com/desafio/person_api/domain/entity/Address.java
+++ Spring/person-api/src/main/java/com/desafio/person_api/domain/entity/Address.java
@@ -1,14 +1,18 @@
 package com.desafio.person_api.domain.entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
 @Entity(name = "address")
@@ -23,12 +27,29 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID) // pensar se precisa de uuid ou se pode ser um id numérico
     private String id;
- 
+    
+    @Column(nullable = false)
     private String rua;
+
+    @Column(nullable = false)
     private String numero;
+    
+    @Column(nullable = false)
     private String bairro;
+
+    @Column(nullable = false)
     private String cidade;
+
+    @Column(nullable = false)
     private String estado;
+
+    @Column(nullable = false)
     private String cep;
 
+    @Column(nullable = false)
+    private boolean principal;
+
+    @ManyToOne(fetch = FetchType.LAZY) // para evitar carregamento desnecessário da pessoa quando buscamos um endereço
+    @JoinColumn(name = "person_id") 
+    private Person person; 
 }
--- Spring/person-api/src/main/java/com/desafio/person_api/domain/entity/Person.java
+++ Spring/person-api/src/main/java/com/desafio/person_api/domain/entity/Person.java
@@ -1,14 +1,19 @@
 package com.desafio.person_api.domain.entity;
 
 import java.time.LocalDate;
-
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.validator.constraints.br.CPF;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,12 +29,23 @@ public class Person {
     
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id; // cara deixei como string
+    private String id; 
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(name = "data_nascimento", nullable = true)
     private LocalDate dataNascimento;
 
+    @Column(nullable = false, unique = true)
     private String cpf;
+
+    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>(); 
+
+    public void addAddress(Address address) {
+        addresses.add(address);
+        address.setPerson(this);
+    }
 
 }
